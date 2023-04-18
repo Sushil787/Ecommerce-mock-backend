b22a5c744df28e6054e266252e8f18b1453bf21d
Sushil787 <itsushil100@outlook.com>
2023-04-18 21:41:34 +0545

    completed: product crud

--- controller/product_controller.js
+++ controller/product_controller.js
@@ -13,10 +13,52 @@ const add_product = async (req, res) => {
       categories,
       price,
     });
-    return res.status(200)
-.json(new_product)  } catch (error) {
+    return res.status(200).json(new_product);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+const get_product = async (req, res) => {
+  try {
+    const all_product = await product.find({});
+    return res.status(200).json(all_product);
+  } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 };
 
-module.exports = add_product;
+const update_product = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { title, image, decs, categories, price } = req.body;
+    if (!title || !image || !decs || !categories || !price) {
+      return res.status(400).json({ message: "no complete data" });
+    }
+    const updated_product = await product.findByIdAndUpdate({_id:id},{title, image, decs, categories, price},{new:true});
+    return res.status(200).json(updated_product);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+};
+
+const delete_product = async (req, res) => {
+  try {
+    const product_id = req.params.id;
+    console.log(product_id);
+   const id = await product.findById({_id:product_id});
+    if (!id) {
+      return res.status(400).json({ message: "no product with such id"});
+    }
+    await product.findByIdAndDelete({_id:product_id});
+    return res.status(200).json({success:"Deleted product successfully"});
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+module.exports = {
+  get_product,
+  update_product,
+  delete_product,
+  add_product,
+};
--- router/app_router.js
+++ router/app_router.js
@@ -2,17 +2,25 @@ const express = require("express");
 const auth_middleware = require("../middleware/auth_middleware");
 const { login, signup } = require("../controller/auth_controller");
 const { get_cart, add_cart } = require("../controller/cart_controller");
-const add_product = require("../controller/product_controller");
+const {add_product, get_product, update_product, delete_product} = require("../controller/product_controller");
 const app_router = express.Router();
 
+/**  auth route bloc */
 app_router.post("/user/login", login);
 app_router.post("/user/signup", signup);
+
+
+/** cart route bloc */
 app_router.post("/cart/get", auth_middleware, get_cart);
 app_router.post("/cart/create", auth_middleware, add_cart);
-// app_router.post("/cart/add", auth_middleware);
-// app_router.get("/product/get", auth_middleware);
+app_router.post("/cart/create", auth_middleware, add_cart);
+app_router.post("/cart/create", auth_middleware, add_cart);
+
+
+/** product route bloc */
+app_router.get("/product/get", auth_middleware, get_product);
 app_router.post("/product/create", auth_middleware, add_product);
-// app_router.put("/product/update", auth_middleware);
-// app_router.delete("/product/delete/:id", auth_middleware);
+app_router.put("/product/update/:id", auth_middleware, update_product);
+app_router.delete("/product/delete/:id", auth_middleware, delete_product);
 
 module.exports = app_router;
