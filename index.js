bc0ae7383d83d36beab29b0190f12c7fec5fa66f
sushil gyawali <63179543+Sushil787@users.noreply.github.com>
2023-04-26 12:44:23 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const appRouterVerson1 = require("./router/V1.0/app_router");
 const appRouterVersion2 = require("./router/V1.1/app_router");
-// const cors = require("cors");
+const cors = require("cors");
 const morgan = require('morgan');
 require("dotenv").config();
 const mongoose_connection = require("./db/connection");
 const app = express();
-// app.use(cors({
-//     origin:"www.xyz.com",
-//     methods:['GET','POST','PUT','DELETE'],
-// }));
+app.use(cors({
+    origin:"www.xyz.com",
+    methods:['GET','POST','PUT','DELETE'],
+}));
 mongoose_connection(app);
 
 app.use(express.urlencoded({ extended: true }));
